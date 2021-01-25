61de9ec6e4068bda15d69b79aded2b0aaff30f20
Paweł Stawicki <stawel+chealiCharger@gmail.com>
2021-01-25 23:08:14 +0100

    add debug flag

--- ht301.py
+++ ht301.py
@@ -3,16 +3,16 @@ import numpy as np
 import math
 
 
+debug = 2
 
 def f32(m3, idx):
     v = m3[idx:idx+4].view(dtype=np.dtype(np.float32))
-#   print(v)
     return float(v[0])
 
 def u16(m3, idx):
     v = m3[idx:idx+4].view(dtype=np.dtype(np.uint16))
-#   print(v)
     return int(v[0])
+
 
 Fix_, Distance_, refltmp_, airtmp_, Humi_, Emiss_ = 0., 0., 0., 0., 0., 0.
 fpatmp_, fpaavg_, orgavg_, coretmp_ = 0., 0., 0., 0.
@@ -52,8 +52,10 @@ def sub_10001180(coretmp_, cx):
     d_ = -Distance_**0.5
     w_ = w ** 0.5
     t =  K_atm * math.exp(d_ * (a1 + b1 * w_)) + (1. - K_atm) * math.exp(d_ * (a2 + b2 * w_))
-    print('water vapour content coefficient:', w)
-    print('transmittance of atmosphere:     ', t)
+
+    if debug > 0:
+        print('water vapour content coefficient:', w)
+        print('transmittance of atmosphere:     ', t)
 
     part_emi_t_1 = 1.0 / (Emiss_ * t)
     part_Tatm_Trefl = (1.0 - Emiss_) * t * (refltmp_ - ABSOLUTE_ZERO_CELSIUS)**4  +  (1.0 - t) * (airtmp_ - ABSOLUTE_ZERO_CELSIUS)**4
@@ -94,10 +96,10 @@ def sub_10001180(coretmp_, cx):
 
 #fpa <- focal-plane array (sensor)
 
-def temperatureLut(meta0, meta3):
+def temperatureLut(fpatmp_, meta3):
 
     global Fix_, Distance_, refltmp_, airtmp_, Humi_, Emiss_
-    global fpatmp_, fpaavg_, orgavg_, coretmp_ 
+    global fpaavg_, orgavg_, coretmp_ 
 
     global part_emi_t_1, part_Tatm_Trefl
     global flt_10003360, flt_1000335C, flt_1000339C, flt_100033A4, flt_10003398
@@ -107,7 +109,6 @@ def temperatureLut(meta0, meta3):
 
     v5 = meta3[0];
     coretmp_ = float(meta3[1]) / 10.0 + ABSOLUTE_ZERO_CELSIUS;
-    fpatmp_ = 20.0 - (float(meta0[1]) - 7800) / 36.0;
 
     flt_10003360 = f32(m3, 6);
     flt_1000335C = f32(m3, 10);
@@ -125,75 +126,78 @@ def temperatureLut(meta0, meta3):
         Distance_ = u16(m3,127*2 + 20);
         #readParaFromDevFlag = 0;
 
-    print('Fix_',Fix_)
-    print('refltmp_',refltmp_)
-    print('airtmp_',airtmp_)
-    print('Humi_',Humi_)
-    print('Emiss_',Emiss_)
-    print('Distance_',Distance_)
+    if debug > 0:
+        print('Fix_',Fix_)
+        print('refltmp_',refltmp_)
+        print('airtmp_',airtmp_)
+        print('Humi_',Humi_)
+        print('Emiss_',Emiss_)
+        print('Distance_',Distance_)
 
-    print()
-    print('v5',v5)
-    print('coretmp_',coretmp_, meta3[1])
-    print('fpatmp_',fpatmp_, meta0[1])
-    print()
-    print('flt_10003360',flt_10003360)
-    print('flt_1000335C',flt_1000335C)
-    print('flt_1000339C',flt_1000339C)
-    print('flt_10003398',flt_10003398)
-    print('flt_10003394',flt_10003394)
+        print()
+        print('v5',v5)
+        print('coretmp_',coretmp_, meta3[1])
+        print()
+        print('flt_10003360',flt_10003360)
+        print('flt_1000335C',flt_1000335C)
+        print('flt_1000339C',flt_1000339C)
+        print('flt_10003398',flt_10003398)
+        print('flt_10003394',flt_10003394)
 
     return sub_10001180(coretmp_, v5); #//bug in IDA
 
 
 def info(frame):
 
-    print('shape:',frame.shape)
     Height_, Width_ = 288, 384
     meta = frame[Height_:, ...]
-    meta = meta.reshape(4,384)
+    meta = meta.reshape(4, Width_)
     meta0, meta3 = meta[0], meta[3]
 
-    temperatureLUT = temperatureLut(meta0, meta3)
+    Tfpa_raw = meta0[1]
+    fpatmp_ = 20.0 - (float(Tfpa_raw) - 7800.0) / 36.0;
 
-#    print('meta0 :',meta0.tolist())
-#    print('meta12:',meta[1:2].tolist())
-#    print('meta3 :',meta3.tolist())
-#    print(float(meta0[1]))
-    fpatmp_ = 20.0 - (float(meta0[1]) - 7800.0) / 36.0;
-    print('fpatmp_:', fpatmp_)
-    fpaavg_ = meta0[0]
-    orgavg_ = meta0[8];
-    coretmp_ = float(meta3[1]) / 10.0 - 273.1;
+    temperature_LUT_C = temperatureLut(fpatmp_, meta3)
 
-    Fix_ = 0.
-    centertmp = temperatureLUT[meta0[12]] + Fix_;
-    maxtmp = temperatureLUT[meta0[4]] + Fix_;
-    mintmp = temperatureLUT[meta0[7]] + Fix_;
-    maxx = meta0[2];
-    maxy = meta0[3];
-    minx = meta0[5];
-    miny = meta0[6];
+    fpaavg_  = meta0[0]
+#   Tfpa_raw = meta0[1]
+    Tmax_x   = meta0[2]
+    Tmax_y   = meta0[3]
+    Tmax_raw = meta0[4]
+    Tmin_x   = meta0[5]
+    Tmin_y   = meta0[6]
+    Tmin_raw = meta0[7]
 
-    print('m:',maxx, maxy, minx, miny)
-    print('orgavg_:',orgavg_, 'coretmp_:', coretmp_)
-    print('fpaavg_:',fpaavg_)
-    print('centtemp_raw:', meta0[12])
-    print('max_tmp_raw:', meta0[4])
-    print('min_tmp_raw:', meta0[7])
+    orgavg_  = meta0[8]
 
-    print('arr0_tmp_raw:', meta0[13])
-    print('arr1_tmp_raw:', meta0[14])
-    print('arr2_tmp_raw:', meta0[15])
+    Tcenter_raw = meta0[12]
+    Tarr0_raw = meta0[13]
+    Tarr1_raw = meta0[14]
+    Tarr2_raw = meta0[15]
 
-    print('mintmp:', mintmp, 'maxtmp', maxtmp)
-    print('centertmp:',centertmp)
-    centertmp
+    r_info = {
+        'Tmin_C': temperature_LUT_C[Tmin_raw],
+        'Tmin_point': (Tmin_x, Tmin_y),
+        'Tmax_C': temperature_LUT_C[Tmax_raw],
+        'Tmax_point': (Tmax_x, Tmax_y),
+        'Tcenter_C': temperature_LUT_C[Tcenter_raw],
+    }
 
-    print('')
-    #tmparr = temperatureLUT[v13[13]] + Fix_;
-    #tmparr[1] = temperatureLUT[v13[14]] + Fix_;
-    #tmparr[2] = temperatureLUT[v13[15]] + Fix_;
+    if debug > 1:
+        print('meta0 :',meta0.tolist())
+        if debug > 2:  print('meta12:',meta[1:2].tolist())
+        print('meta3 :',meta3.tolist())
+
+    if debug > 0:
+        print('fpatmp_:',fpatmp_,Tfpa_raw)
+        print('fpaavg_:',fpaavg_)
+        print('orgavg_:',orgavg_)
+        print('TarrX_raw:',Tarr0_raw, Tarr1_raw, Tarr2_raw)
+
+        for k in r_info:
+            print(k+':',r_info[k])
+
+    return r_info, temperature_LUT_C
 
     
 
