ddff0d54f91399994c37080a0750becee5bbc92a
Paweł Stawicki <stawel+chealiCharger@gmail.com>
2021-01-30 02:08:34 +0100

    set window title

--- pyplot.py
+++ pyplot.py
@@ -24,6 +24,7 @@ ret, frame = cap.read()
 info, lut = cap.info()
 
 fig = plt.figure()
+fig.canvas.set_window_title('HT301')
 ax = plt.gca()
 im = ax.imshow(lut[frame],cmap=cmaps[cmaps_idx])
 divider = make_axes_locatable(ax)
