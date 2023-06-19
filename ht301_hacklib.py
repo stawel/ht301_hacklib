75069db383d5b23f019327d0b9135c6977018076
Thijs Damsma <thijs.damsma@ceadgroup.com>
2023-06-19 14:46:52 +0200

    add some comments

--- ht301_hacklib.py
+++ ht301_hacklib.py
@@ -362,7 +362,9 @@ class T2SPLUS(HT301):
         return info(self.meta, self.device_strings, height, width, meta_mapping=[0,1])
     
     def temperature_range_normal(self):
+        """Switch camera to the normal temperature range (0°C to 120°C)"""
         self.cap.set(cv2.CAP_PROP_ZOOM, 0x8020)
 
-    def temperature_range_higg(self):
+    def temperature_range_high(self):
+        """Switch camera to the high temperature range (0°C to 400°C)"""
         self.cap.set(cv2.CAP_PROP_ZOOM, 0x8021)
